179755d97f01763646117fc75f5a3d06d5d75440
Jonathan hannerfeldt <hannerfeldt@hotmail.se>
2020-08-24 14:13:43 +0200

    axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+
+  useEffect(() => {
+    axios.post(`http://localhost:8000`).then((res) => {
+      console.log(res.data);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form action="/" method="POST">
+        <button type="submit">klicka här!</button>
+      </form>
     </div>
   );
 }
