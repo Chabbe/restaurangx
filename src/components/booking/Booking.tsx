5c42488f024414524a71faa95c9dd57d192d3d53
Chabbe <henriksson.charlie@gmail.com>
2020-08-26 12:01:44 +0200

    added guestmodel and reservation function and form functionality

--- src/components/booking/Booking.tsx
+++ src/components/booking/Booking.tsx
@@ -3,22 +3,35 @@ import axios from "axios";
 import Guest from "../guest/Guest";
 import Table from "../table/Table";
 import TableModel from "../../models/tableModel";
+import GuestModel from "../../models/guestModel";
 
 export default function Booking() {
   const [tables, setTables] = useState([]);
-
+  const [guests, setGuests] = useState([]);
+  
   useEffect(() => {
     axios.get("http://localhost:8000/table").then((res) => {
       console.log(res.data);
       setTables(res.data);
     });
+
   }, []);
+  useEffect(() => {
+    axios.get("http://localhost:8000/guest").then((res) => {
+      console.log(res.data);
+      setGuests(res.data);
+    });
+  }, []);
+
+  function postBooking(guestObject: GuestModel){
+    axios.post("http://localhost:8000/guest", guestObject);
+  }
 
   return (
     <div>
       <form>
-        <Guest></Guest>
         <Table data={tables}></Table>
+        <Guest data={guests} post={postBooking}></Guest>
       </form>
     </div>
   );
---
+++ src/models/guestModel.ts
@@ -0,0 +1,12 @@
+
+
+type TableModel = {
+ 
+    firstname: String,
+    lastname: String,
+    email: String,
+    phonenr: Number,
+    
+}
+
+export default TableModel;
