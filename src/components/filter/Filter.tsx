cdfda56b70382109e9d5e8d26d31ee4aa11f57f3
Jonathan <Hannerfeldt@hotmail.se>
2020-09-02 14:26:59 +0200

    added first and lastname search

--- src/components/filter/Filter.tsx
+++ src/components/filter/Filter.tsx
@@ -3,28 +3,41 @@ import axios from "axios";
 
 import TableModel from "../../models/tableModel";
 
-interface IFilter{
-    sendData: (data: any)=> void 
+interface IFilter {
+    sendData: (data: any) => void
 }
 
-export default function Filter(props: IFilter){
+export default function Filter(props: IFilter) {
     const [filterDate, setfilterDate] = useState(new Date())
-  
-     useEffect(()=>{
-         console.log(filterDate)
-         axios.post("http://localhost:8000/filter", {date: filterDate}).then((res) => {
-              props.sendData(res.data)
-         });
+    const [filterName, setFilterName] = useState({})
+    
+    useEffect(() => {
+        axios.post("http://localhost:8000/filter", { date: filterDate }).then((res) => {
+            props.sendData(res.data)
+        });
     }, [filterDate])
 
-    function filterBookings(e: ChangeEvent <HTMLInputElement>){
-        setfilterDate(new Date(e.target.value))
-    
+    useEffect(()=> {
+        axios.post("http://localhost:8000/findname", {filterName}).then((res) => {
+            axios.post("http://localhost:8000/filter", {data: res.data.id}).then((res)=> {                
+                props.sendData(res.data)
+            })
+        })
+    }, [filterName])
+
+    function filterBookings(e: ChangeEvent<HTMLInputElement>) {
+        if (e.target.name === "date") setfilterDate(new Date(e.target.value))
+        else setFilterName({
+            ...filterName,
+            [e.target.name]: e.target.value
+        })
     }
 
-    return(
+    return (
         <div>
-            <input type="date" name="date" onChange={filterBookings}></input>
+            <input type="date" name="date" onChange={filterBookings} />
+            <input type="text" name="firstname" onChange={filterBookings} />
+            <input type="text" name="lastname" onChange={filterBookings} />
         </div>
     )
 };
