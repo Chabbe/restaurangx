bbee8141659c094f021756f5b767d685ca9f0bf6
Jonathan hannerfeldt <hannerfeldt@hotmail.se>
2020-09-02 21:59:12 +0200

    msg on admin

--- src/components/admin/Admin.tsx
+++ src/components/admin/Admin.tsx
@@ -1,105 +1,113 @@
 import React, { useState, useEffect, ChangeEvent } from "react";
 import axios from "axios";
-import Filter from "../filter/Filter"
+import Filter from "../filter/Filter";
 import TableModel from "../../models/tableModel";
 
 export default function Admin() {
-    const [tableData, setTableData] = useState([]);
-    const [updatedTable, setUpdatedTable] = useState({
-        date: new Date(),
-        time: 0,
-        count: 0,
+  const [tableData, setTableData] = useState([]);
+  const [updatedTable, setUpdatedTable] = useState({
+    date: new Date(),
+    time: 0,
+    count: 0,
+  });
+  const [msg, setMsg] = useState("");
+
+  useEffect(() => {
+    getTableData();
+  }, []);
+
+  function getTableData() {
+    axios.get("http://localhost:8000/table").then((res) => {
+      setTableData(res.data);
     });
+  }
 
-    useEffect(() => {
+  function updateTable(event: ChangeEvent<HTMLInputElement>) {
+    setUpdatedTable({
+      ...updatedTable,
+      [event.target.name]:
+        event.target.name === "date"
+          ? new Date(event.target.value)
+          : parseInt(event.target.value),
+    });
+  }
 
-        getTableData();
+  function unbook(id: number) {
+    axios
+      .delete("http://localhost:8000/unbook/" + id)
+      .then(() => getTableData());
+  }
 
-    }, []);
+  function editBooking(id: number) {
+    axios
+      .post("http://localhost:8000/availability", updatedTable)
+      .then((res) => {
+        if (res.data.success) {
+          axios
+            .put("http://localhost:8000/edit/" + id, updatedTable)
+            .then((res) => getTableData());
+          setMsg("bokning ändrad");
+        } else if (res.data.othersuccess) {
+          console.log("fullbokat men ledigt på other");
+          setMsg("Fullbokat men ledigt på andra tiden");
+        } else {
+          setMsg("fullbokat");
+        }
+      });
+  }
 
-    function getTableData() {
-
-        axios.get("http://localhost:8000/table").then((res) => {
-            setTableData(res.data);
-        });
-
-    }
-
-    function updateTable(event: ChangeEvent<HTMLInputElement>) {
-
-        setUpdatedTable({
-            ...updatedTable,
-            [event.target.name]:
-                event.target.name === "date"
-                    ? new Date(event.target.value)
-                    : parseInt(event.target.value),
-        });
-
-    }
-
-    function unbook(id: number) {
-        axios
-            .delete("http://localhost:8000/unbook/" + id)
-            .then(() => getTableData());
-    }
-
-    function editBooking(id: number) {
-        axios.post("http://localhost:8000/availabilty", updatedTable).then((res) => {
-            if (res.data.success) axios.put("http://localhost:8000/edit/" + id, updatedTable).then((res) => getTableData())
-        })
-    }
-
-    function edit(id: number) {
-        return (
-            <div>
-                <input
-                    type="date"
-                    name="date"
-                    placeholder="date"
-                    onChange={updateTable}
-                />
-                <input
-                    type="text"
-                    name="time"
-                    placeholder="time"
-                    onChange={updateTable}
-                />
-                <input
-                    type="text"
-                    name="count"
-                    placeholder="count"
-                    onChange={updateTable}
-                />
-                <button onClick={() => editBooking(id)}>edit</button>
-            </div>
-        );
-    }
-
-    function getData(data: any) {
-        setTableData(data)
-    };
-
+  function edit(id: number) {
     return (
-        <div>
-            <Filter sendData={getData}></Filter>
-            <ul>
-                {tableData.map((data: TableModel, index) => {
-                    return (
-                        <li key={index}>
-                            id: {data.id} date: {data.date.toString().slice(0, 10)} time:{" "}
-                            {data.time} count: {data.count} guestId: {data.guestId}
-                            <button
-                                onClick={() => {
-                                    unbook(data.id);
-                                }}
-                            >
-                                unbook
-                            </button>
-                            {edit(data.id)}
-                        </li>
-                    );
-                })}
-            </ul>
-        </div>
+      <div>
+        <input
+          type="date"
+          name="date"
+          placeholder="date"
+          onChange={updateTable}
+        />
+        <input
+          type="text"
+          name="time"
+          placeholder="time"
+          onChange={updateTable}
+        />
+        <input
+          type="text"
+          name="count"
+          placeholder="count"
+          onChange={updateTable}
+        />
+        <button onClick={() => editBooking(id)}>edit</button>
+      </div>
     );
+  }
+
+  function getData(data: any) {
+    setTableData(data);
+  }
+
+  return (
+    <div>
+      {msg}
+      <Filter sendData={getData} reset={getTableData}></Filter>
+      <ul>
+        {tableData.map((data: TableModel, index) => {
+          return (
+            <li key={index}>
+              id: {data.id} date: {data.date.toString().slice(0, 10)} time:{" "}
+              {data.time} count: {data.count} guestId: {data.guestId}
+              <button
+                onClick={() => {
+                  unbook(data.id);
+                }}
+              >
+                unbook
+              </button>
+              {edit(data.id)}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
