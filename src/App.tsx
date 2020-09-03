3af43cd2b2fe1ef597241d142efc560ecf65d319
Jonathan hannerfeldt <hannerfeldt@hotmail.se>
2020-09-03 19:21:24 +0200

    neon sign

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
 import React from "react";
-<<<<<<< HEAD
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-=======
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.scss";
->>>>>>> 44f27bed047bc7b943d20cdf654fd0cb9df43727
 import Booking from "./components/booking/Booking";
 import Admin from "./components/admin/Admin";
 //import "./App.css";
@@ -16,18 +8,6 @@ import Home from "./components/home/Home";
 
 function App() {
   return (
-<<<<<<< HEAD
-    <Router>
-      <Switch>
-        <Route path="/admin">
-          <Admin></Admin>
-        </Route>
-        <Route path="/">
-          <Booking></Booking>
-        </Route>
-      </Switch>
-    </Router>
-=======
     <div className="container-fluid">
       <Router>
         <Switch>
@@ -44,7 +24,6 @@ function App() {
         </Switch>
       </Router>
     </div>
->>>>>>> 44f27bed047bc7b943d20cdf654fd0cb9df43727
   );
 }
 
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -3,11 +3,15 @@ import React from "react";
 export default function Home() {
   return (
     <div className="row backgroundWrapper">
-      <div className="col-12">
-        <p className="">Restaurang X</p>
-        <button className="glow-on-hover" >
-          Get a table
-        </button>
+      <div className="align-self-center">
+        <p className="restaurangx">
+          <div id="R">r</div>e<div id="S">s</div>taurang X
+        </p>
+        <div className="row">
+          <button className="align-self-center glow-on-hover">
+            Get a table
+          </button>
+        </div>
       </div>
     </div>
   );
